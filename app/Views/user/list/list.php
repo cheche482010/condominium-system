1bdc351e981846b375596823839cdee01a65ea6e
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2024-10-16 17:29:24 -0400

    agregando archivos

--- app/Views/user/list/list.php
+++ app/Views/user/list/list.php
@@ -1,1 +1,320 @@
-<h1>consultar usuarios</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Torres Music</title>
+    <?php 
+    $this->Link([
+      ['rel' => 'icon', 'href' => $this->assets . 'img/logo.jpg', 'type' => 'image/x-icon'],
+      ['rel' => 'stylesheet', 'href' => 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback'],
+      ['rel' => 'stylesheet', 'href' => $this->assets . 'plugins/fontawesome-free/css/all.min.css'],
+      ['rel' => 'stylesheet', 'href' => 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'],
+      ['rel' => 'stylesheet', 'href' => $this->assets . 'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'],
+      ['rel' => 'stylesheet', 'href' => $this->assets . 'plugins/icheck-bootstrap/icheck-bootstrap.min.css'],
+      ['rel' => 'stylesheet', 'href' => $this->assets . 'css/adminlte.min.css'],
+      ['rel' => 'stylesheet', 'href' => $this->assets . 'plugins/overlayScrollbars/css/OverlayScrollbars.min.css'],
+      ['rel' => 'stylesheet', 'href' => $this->assets . 'plugins/daterangepicker/daterangepicker.css'],
+      ['rel' => 'stylesheet', 'href' => $this->assets . 'plugins/summernote/summernote-bs4.min.css'],
+      ['rel' => 'stylesheet', 'href' => $this->assetsView . "user/list/list.scss"],
+    ])->view();
+  ?>
+
+    <!-- DataTables CSS -->
+    <link href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+
+</head>
+
+<body class="layout-navbar-fixed accent-primary layout-footer-fixed layout-fixed sidebar-mini sidebar-collapse">
+    <div class="wrapper">
+
+        <!-- Preloader -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="<?php echo $this->assets; ?>img/logo.jpg" alt="Logo" height="60"
+                width="60">
+        </div>
+
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-light bg-primary border-bottom-0">
+            <!-- Left navbar links -->
+            <?php
+            $this->NavbarNav([
+                ['url' => '#', 'text' => 'Inicio'],
+                ['url' => '#', 'text' => 'Contacto'],
+            ])->view();
+            ?>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Notifications Dropdown Menu -->
+                <?php
+                $this->Notifications([
+                    [
+                        'icon' => 'far fa-envelope',
+                        'text' => '4 new messages',
+                        'timestamp' => '3 mins',
+                        'link' => '#',
+                    ],
+                    [
+                        'icon' => 'fas fa-users',
+                        'text' => '8 friend requests',
+                        'timestamp' => '12 hours',
+                        'link' => '#',
+                    ],
+                    [
+                        'icon' => 'fas fa-file',
+                        'text' => '3 new reports',
+                        'timestamp' => '2 days',
+                        'link' => '#',
+                    ],
+                ])->view();
+                ?>
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                        <i class="fas fa-expand-arrows-alt"></i>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar elevation-2 sidebar-light-primary">
+            <!-- Brand Logo -->
+            <a href="javascript::void(0)" class="brand-link bg-primary">
+                <img src="<?php echo $this->assets; ?>img/logo.jpg" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">Torres Music</span>
+            </a>
+
+            <!-- Sidebar -->
+            <?php
+                $this->SidebarMenu([
+                    [
+                        'type'  => 'header',
+                        'label' => 'MULTI LEVEL EXAMPLE',
+                    ],
+                    [
+                        'url'   => '#',
+                        'icon'  => 'fas fa-circle nav-icon',
+                        'label' => 'Level 1',
+                    ],
+                    [
+                        'url'      => '#',
+                        'icon'     => 'nav-icon fas fa-circle',
+                        'label'    => 'Level 1',
+                        'subItems' => [
+                            [
+                                'url'   => '#',
+                                'icon'  => 'far fa-circle nav-icon',
+                                'label' => 'Level 2',
+                            ],
+                            [
+                                'url'      => '#',
+                                'icon'     => 'far fa-circle nav-icon',
+                                'label'    => 'Level 2',
+                                'subItems' => [
+                                    [
+                                        'url'   => '#',
+                                        'icon'  => 'far fa-dot-circle nav-icon',
+                                        'label' => 'Level 3',
+                                    ],
+                                    [
+                                        'url'   => '#',
+                                        'icon'  => 'far fa-dot-circle nav-icon',
+                                        'label' => 'Level 3',
+                                    ],
+                                    [
+                                        'url'   => '#',
+                                        'icon'  => 'far fa-dot-circle nav-icon',
+                                        'label' => 'Level 3',
+                                    ],
+                                ],
+                            ],
+                            [
+                                'url'   => '#',
+                                'icon'  => 'far fa-circle nav-icon',
+                                'label' => 'Level 2',
+                            ],
+                        ],
+                    ],
+                    [
+                        'url'   => '#',
+                        'icon'  => 'fas fa-circle nav-icon',
+                        'label' => 'Level 1',
+                    ],
+                ])->view();
+            ?>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <?php
+            $this->ContentHeader([
+                'titulo' =>  $this->config->get('APP_NAME'),  
+                'links' => [
+                    ['label' =>  $this->config->get('APP_NAME'), 'url' => '#','active' => true],
+                    ['label' => 'Inicio', 'url' => '#'],
+                ],
+            ])->view();
+            ?>
+
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table id="userTable" class="table table-hover table-bordered" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Last Name</th>
+                                        <th>CI</th>
+                                        <th>Phone</th>
+                                        <th>Email</th>
+                                        <th>Rol</th>
+                                        <th>Is Active</th>
+                                        <th>Created At</th>
+                                        <th>Updated At</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <?php 
+            $this->Footer([
+            'siteName' =>  $this->config->get('APP_NAME'),
+            'author' => 'SETHAR',
+            'year' => '2023',
+            'version' => '1.0',
+            ])->view(); 
+        ?>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <?php 
+        $this->Script([
+        $this->assets . 'plugins/jquery/jquery.min.js',
+        $this->assets . 'plugins/jquery-ui/jquery-ui.min.js',
+        $this->assets . 'plugins/bootstrap/js/bootstrap.bundle.min.js',
+        $this->assets . 'plugins/chart.js/Chart.min.js',
+        $this->assets . 'plugins/jquery-knob/jquery.knob.min.js',
+        $this->assets . 'plugins/moment/moment.min.js',
+        $this->assets . 'plugins/daterangepicker/daterangepicker.js',
+        $this->assets . 'plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
+        $this->assets . 'plugins/summernote/summernote-bs4.min.js',
+        $this->assets . 'plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+        $this->assets . 'js/adminlte.js',
+        $this->assets . 'js/constants.js',
+        $this->assets . 'js/function.js',
+        ])->view(); 
+    ?>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+
+</body>
+
+</html>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- DataTables -->
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+$(document).ready(function() {
+
+    // Initialize DataTable with AJAX
+    $('#userTable').DataTable({
+        ajax: PROJECT_URL + '/api/user/getAll',
+        columns: [{
+                data: 'id'
+            },
+            {
+                data: 'nombre'
+            },
+            {
+                data: 'apellido'
+            },
+            {
+                data: 'cedula'
+            },
+            {
+                data: 'phone'
+            },
+            {
+                data: 'email'
+            },
+            {
+                data: 'rol'
+            },
+            {
+                data: 'is_active'
+            },
+            {
+                data: 'created_at'
+            },
+            {
+                data: 'updated_at'
+            }
+        ],
+        columnDefs: [{
+                targets: '_all',
+                orderable: true
+            },
+            {
+                className: 'text-center',
+                targets: '_all'
+            }
+        ],
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
+        },
+        responsive: true,
+        scrollX: true,
+        scrollY: '400px',
+        scrollCollapse: true,
+        paging: true,
+        info: true,
+        autoWidth: false,
+        dom: 'Blfrtip',
+        buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
+        ordering: true,
+        processing: true,
+        pageLength: 10,
+        lengthMenu: [5, 10, 20, 30, 40, 50, 100]
+    });
+
+    // Handle server-side processing
+    $('#userTable').on('processing.dt', function(e, settings, processing) {
+        if (processing) {
+            $(this).find('.dataTables_processing').addClass(
+                'd-flex justify-content-center align-items-center');
+        } else {
+            $(this).find('.dataTables_processing').removeClass(
+                'd-flex justify-content-center align-items-center');
+        }
+    });
+});
+</script>
