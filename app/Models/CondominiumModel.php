b8101fbf3e6e71ead8cc9abe4f880e23e1a27acb
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2024-10-17 21:59:05 -0400

    modulo base condominios

---
+++ app/Models/CondominiumModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+class CondominiumModel extends BaseModel
+{
+    public string $id; 
+    public string $nombre; 
+    public string $deuda; 
+    public string $alicuota; 
+    public string $is_active;
+
+    private $sql;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->sql = [
+            'getAll' => "",
+            'getById' => "",
+            'create' => "",
+            'update' => "",
+            'delete' => "",
+        ];               
+    }
+
+    public function execute($sqlKey, $params = [], $fetchOption = "all")
+    {
+        if (!array_key_exists($sqlKey, $this->sql)) {
+            throw new \InvalidArgumentException("Clave SQL no válida: $sqlKey");
+        }
+
+        $sanitizedParams = array_map(function ($param) {
+            return $this->sanitize($param);
+        }, $params);
+        
+        if ($fetchOption === "create" || $fetchOption === "update" || $fetchOption === "delete") {
+            return $this->db->executeQuery($this->sql[$sqlKey], $sanitizedParams);
+        } elseif ($fetchOption === "single" || $fetchOption === "all") {
+            return $this->db->getResults($this->sql[$sqlKey], $sanitizedParams, $fetchOption);
+        } else {
+            throw new \InvalidArgumentException("Opción de obtención no válida: $fetchOption");
+        }
+    }
+}
