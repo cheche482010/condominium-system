2dfed928fcc0ca1b61260fdb7c155076060bd163
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2024-10-18 21:55:08 -0400

    documentacion swagger

--- app/Documentation/api.php
+++ app/Documentation/api.php
@@ -1,6 +1,10 @@
 <?php
 require("../../vendor/autoload.php");
 
-$openapi = \OpenApi\Generator::scan(["../../app/Controllers"]);
+$openapi = \OpenApi\Generator::scan([
+    "../../app/Controllers",
+    "../../app/Controllers/Docs/UserDocumentation.php"
+]);
+
 header('Content-Type: application/json');
 echo $openapi->toJSON();
