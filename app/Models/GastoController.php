30515ef57d82d59ed5dc1c02237502b4a6d654ff
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2024-10-20 15:44:07 -0400

    gasto  modulo

---
+++ app/Models/GastoController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controllers;
+
+use OpenApi\Annotations as OA;
+
+class GastoController extends BaseController
+{
+    private $datos;
+    private $respuesta;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->respuesta = [];
+        $this->datos = isset($_POST) ? $_POST : [];
+    }
+
+  
+    public function getAll()
+    {
+        $this->isGetRequest();
+    }
+
+    public function getById($id)
+    {
+        $this->isGetRequest();
+    }
+
+    public function create()
+    {
+        $this->isPostRequest();
+    }
+
+    public function update($id)
+    {
+        $this->isPutRequest();
+    }
+
+    private function validateCondominiumData($data)
+    {
+
+        $rules = [
+            'nombre' => 'required|regex:cedula|min:6|max:9',
+        ];
+
+        $errors = $this->validator->validate($data, $rules);
+
+        return  $errors ?? null;
+    }
+
+    public function renderView($viewName)
+    {
+        try {
+            require __DIR__ . "/../Views/gasto/{$viewName}/{$viewName}.php";
+        } catch (\Exception $e) {
+            throw new \Exception('Error al cargar la vista: ' . $e->getMessage());
+        }
+    }
+}
