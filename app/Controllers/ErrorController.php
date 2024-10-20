ea56a051df77df1135cb41f40ef7ff50a26f807c
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2024-10-20 17:26:40 -0400

    error view

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -4,14 +4,26 @@ namespace App\Controllers;
 
 class ErrorController
 {
+    private function handleErrorCode($statusCode)
+    {
+        try {
+            $filePath = __DIR__ . "/../Views/error/$statusCode.php";
+            http_response_code($statusCode);
+            require $filePath;
+        } catch (\Exception $e) {
+            http_response_code(500);
+            throw new \Exception("Error: Archivo de error no encontrado: $filePath" . $e->getMessage());
+        }
+    }
+
     public function __call($name, $arguments)
     {
         if (strpos($name, '_') === 0) {
             $statusCode = intval(substr($name, 1));
             if ($statusCode >= 400 && $statusCode < 600) {
-                http_response_code($statusCode);
-                include_once __DIR__ . "/../Views/error/$statusCode.php";
+                $this->handleErrorCode($statusCode);
             }
         }
     }
+
 }
