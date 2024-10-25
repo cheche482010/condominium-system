c95dd6e753b43bce8aaf963145297422eeed8796
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2024-10-25 15:20:39 -0400

    datos bancos

---
+++ core/Database/Sql/Data.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+    bancos (codigo, nombre, is_active)
+VALUES
+    ('0102', 'Banco de Venezuela', 1),
+    ('0105', 'Banco Mercantil', 1),
+    ('0106', 'Banco Provincial', 1),
+    ('0107', 'Banco Bicentenario', 1),
+    ('0108', 'Banco Occidental', 1),
+    ('0110', 'Banco Universal', 1),
+    ('0111', 'Banco Plaza', 1),
+    ('0112', 'Banco Venezzuela (Banco Industrial)', 1),
+    ('0113', 'Banco Exterior', 1),
+    ('0114', 'Banco Latino', 1),
+    ('0115', 'Banco Nacional de Crédito Cooperativo', 1),
+    ('0116', 'Banco Agrícola Latino', 1),
+    ('0117', 'Banco Comercial Latino', 1),
+    ('0118', 'Banco Continental', 1),
+    ('0119', 'Banco Caribe', 1);
