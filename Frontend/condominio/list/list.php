4b8876c83fc9394ef3e21df29dd31ed765e0e03a
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2024-10-31 21:22:41 -0400

    vista listar condominio

--- Frontend/condominio/list/list.php
+++ Frontend/condominio/list/list.php
@@ -3,10 +3,10 @@
 
 <head>
     <?php include __DIR__ . "/../../includes/meta.php"; ?>
-    <title><?= $this->config->get('APP_NAME'); ?></title>
+    <title><?= TITLE; ?></title>
     <?php include __DIR__ . "/../../includes/link.php"; ?>
 
-    <link href="<?= $this->assetsView; ?>condominio/list/list.scss" rel="stylesheet">
+    <link href="<?= URL; ?>Frontend/condominio/list/list.scss" rel="stylesheet">
     <?php include __DIR__ . "/../../includes/data-table.php"; ?>
 </head>
 
@@ -24,16 +24,21 @@
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
             <!-- Content Header (Page header) -->
-            <?php
-            $this->ContentHeader([
-                'titulo' =>  "Condominios" , 
-                'links' => [
-                    ['label' =>  "Condominios", 'url' => '#','active' => true],
-                    ['label' => 'Listar', 'url' => 'javascript::void(0)'],
-                ],
-            ])->view();
-            ?>
-
+            <div class='content-header'>
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Condominios</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item active">Condominium System</li>
+                                <li class="breadcrumb-item"><a href="list">Listar</a></li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
             <!-- /.content-header -->
 
             <!-- Main content -->
@@ -87,7 +92,7 @@
     <!-- ./wrapper -->
     <?php include __DIR__ . "/../../includes/script.php"; ?>
 
-    <script src="<?= $this->assetsView; ?>condominio/list/list.js"></script>
+    <script src="<?= URL; ?>Frontend/condominio/list/list.js"></script>
 </body>
 
 </html>
