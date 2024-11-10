0779ea5a928227bb9c7e497b395ac35e655485b8
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2024-11-10 17:42:34 -0400

    cambios bd

--- Api/core/Database/Sql/Data.sql
+++ Api/core/Database/Sql/Data.sql
@@ -1,21 +1,21 @@
 INSERT INTO
-    bancos (codigo, nombre, is_active)
+    bancos (codigo, nombre)
 VALUES
-    ('0102', 'Banco de Venezuela', 1),
-    ('0105', 'Banco Mercantil', 1),
-    ('0106', 'Banco Provincial', 1),
-    ('0107', 'Banco Bicentenario', 1),
-    ('0108', 'Banco Occidental', 1),
-    ('0110', 'Banco Universal', 1),
-    ('0111', 'Banco Plaza', 1),
-    ('0112', 'Banco Venezzuela (Banco Industrial)', 1),
-    ('0113', 'Banco Exterior', 1),
-    ('0114', 'Banco Latino', 1),
-    ('0115', 'Banco Nacional de Crédito Cooperativo', 1),
-    ('0116', 'Banco Agrícola Latino', 1),
-    ('0117', 'Banco Comercial Latino', 1),
-    ('0118', 'Banco Continental', 1),
-    ('0119', 'Banco Caribe', 1);
+    ('0102', 'Banco de Venezuela'),
+    ('0105', 'Banco Mercantil'),
+    ('0106', 'Banco Provincial'),
+    ('0107', 'Banco Bicentenario'),
+    ('0108', 'Banco Occidental'),
+    ('0110', 'Banco Universal'),
+    ('0111', 'Banco Plaza'),
+    ('0112', 'Banco Venezzuela (Banco Industrial)'),
+    ('0113', 'Banco Exterior'),
+    ('0114', 'Banco Latino'),
+    ('0115', 'Banco Nacional de Crédito Cooperativo'),
+    ('0116', 'Banco Agrícola Latino'),
+    ('0117', 'Banco Comercial Latino'),
+    ('0118', 'Banco Continental'),
+    ('0119', 'Banco Caribe');
 
 INSERT INTO 
     condominios (id_website, nombre, deuda, alicuota)
